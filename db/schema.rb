be8e1b6e8ec326acf0102d073446206f3e26de50
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-12-10 10:44:38 +0300

    Configure for Render

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_09_085230) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "airlines", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_085230) do
     t.string "title"
     t.string "description"
     t.integer "score"
-    t.integer "airline_id", null: false
+    t.bigint "airline_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["airline_id"], name: "index_reviews_on_airline_id"
